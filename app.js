5282e704e055cf4d3a8f6d4910d97da5c6e7b7f2
avinav291 <avinav291@gmail.com>
2017-03-26 18:17:18 +0530

    App Chnaged for openshift Deploy

--- app.js
+++ app.js
@@ -33,15 +33,65 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 
-//Database
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+// //Database
+// var mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/test');
 
-var uri = "mongodb://username:password@ds137360.mlab.com:37360/throughputdemo";
-var connect = function(callback){
-  mongoose.connect(uri, callback);
+// var uri = "mongodb://username:password@ds137360.mlab.com:37360/throughputdemo";
+// var connect = function(callback){
+//   mongoose.connect(uri, callback);
+// };
+
+//NEW DB Changes
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+        mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+        mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+        mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+        mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+    mongoUser = process.env[mongoServiceName + '_USER'];
+
+    if (mongoHost && mongoPort && mongoDatabase) {
+        mongoURLLabel = mongoURL = 'mongodb://';
+        if (mongoUser && mongoPassword) {
+            mongoURL += mongoUser + ':' + mongoPassword + '@';
+        }
+        // Provide UI label that excludes user id and pw
+        mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+        mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+    }
+}
+var db = null,
+    dbDetails = new Object();
+
+var initDb = function(callback) {
+    if (mongoURL == null) return;
+
+    var mongodb = require('mongodb');
+    if (mongodb == null) return;
+
+    mongoose.connect(mongoURL, function(err, conn) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        db = conn;
+        dbDetails.databaseName = db.databaseName;
+        dbDetails.url = mongoURLLabel;
+        dbDetails.type = 'MongoDB';
+
+        console.log('Connected to MongoDB at: %s', mongoURL);
+    });
 };
+
 //
 //Static Directories
 app.use(express.static(path.join(__dirname, 'views')));
@@ -103,4 +153,4 @@ function normalizePort(val) {
 //
 
 
-// module.exports = app;
+module.exports = app;
